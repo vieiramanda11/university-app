d0fe03f7361d418ef6d4294e219d46a595506782
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-25 18:19:45 -0300

    creating partial to render errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,24 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "You have successfully sign up."
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def student_params
+      params.require(:student).permit(:name, :email)
+    end
 end
