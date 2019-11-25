c88da48a17065d6f39fbdbf22edd6c7a75701491
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-25 16:27:44 -0300

    creating user model and add validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,4 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: { minimum: 5 }
   validates :name, presence: true, length: { minimum: 3 }
   validates :description, presence: true, length: { minimum: 15 }
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_190605) do
+ActiveRecord::Schema.define(version: 2019_11_25_192555) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
