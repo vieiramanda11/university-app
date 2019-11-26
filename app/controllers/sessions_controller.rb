b7546875cec0e39a06571f67cc7191804c55b56b
vieiramanda11 <amanda_vieira@outlook.com>
2019-11-26 13:23:36 -0300

    creating actions for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    student = Student.find_by(email: params[:sessions][:email].downcase)
+    if student && student.authenticate(params[:sessions][:password])
+      session[:student_id] = student.id
+      flash[:notice] = "Log in successfully"
+      redirect_to student
+    else
+      flash.now[:notice] = "Something was wrong with your login information"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:student_id] = nil
+    flash[:notice] = "Logout successfully"
+    redirect_to root_path
   end
 end
